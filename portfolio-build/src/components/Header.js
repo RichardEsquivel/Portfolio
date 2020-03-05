c86e25ff4ab7461cbd9fe7e9bcbe9749e4989380
RichardEsquivel <rlesquivel@gmail.com>
2020-03-04 22:09:02 -0600

    Added logo after adding styled components and react-router-dom package

--- portfolio-build/src/components/Header.js
+++ portfolio-build/src/components/Header.js
@@ -1,1 +1,20 @@
-import re-logo from "../src/images/portfolio-images/re-logo.png";
+import React from 'react';
+import relogo from "../../src/images/portfolio-images/re-logo.png";
+import styled from "styled-components";
+
+const MainHeader = styled.header`
+	margin: 0 auto;
+	width: 100%;
+`;
+
+export default function Header() {
+	return (
+		<>
+			<MainHeader>
+				<img src={relogo} alt="logo" />
+			</MainHeader>
+
+
+		</>
+	)
+}
