690ffcd094c31d069702ff48fbc758836d97dbe0
RichardEsquivel <rlesquivel@gmail.com>
2020-05-08 17:24:51 -0500

    Commiting to my portfolio some more routes added

--- portfolio-build/src/components/Header.js
+++ portfolio-build/src/components/Header.js
@@ -2,16 +2,35 @@ import React from 'react';
 import relogo from "../../src/images/portfolio-images/re-logo.png";
 import styled from "styled-components";
 
+//styling for the main header
 const MainHeader = styled.header`
 	margin: 0 auto;
 	width: 100%;
+	/* styling for collapsible toggle menu */
+	.nav-toggle{
+		
+	}
+	.hamburgerMenu{
+
+	}
 `;
 
 export default function Header() {
 	return (
 		<>
 			<MainHeader>
-				<img src={relogo} alt="logo" />
+				<img src={relogo} alt="RE logo" />
+				<button className="nav-toggle" aria-label="toggle-navigation">
+					<span className="hamburgerMenu"></span>
+				</button>
+				<nav className="nav">
+					<ul className="nav__list">
+						<li class="nav__item"> <a href="#" class="nav__link">Home</a></li>
+						<li class="nav__item"> <a href="#" class="nav__link">My Work</a></li>
+						<li class="nav__item"> <a href="#" class="nav__link">About Me</a></li>
+
+					</ul>
+				</nav>
 			</MainHeader>
 
 
---
+++ portfolio-build/src/components/Home.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+import styled from "styled-components";
+
+const StyledMain = styled.main`
+
+
+`;
+
+
+export default function Home() {
+
+}
+
+
+return (
+	<>
+		<div>
+			<StyledMain>
+				<section className="intro" id="home">
+
+				</section>
+
+			</StyledMain>
+
+		</div>
+
+	</>
+)
